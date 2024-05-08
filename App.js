2db77e22b71cfc82516a112630df6561b5f60c32
binhm <binhmai@Binhs-MacBook-Pro.local>
2024-05-07 19:38:46 -0700

    set up

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './App/Navigation/Screens/HomeScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>What are you making today?</Text> */}
+      <HomeScreen/>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +16,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
---
+++ Utils/Colors.js
@@ -0,0 +1,5 @@
+export default {
+    BLACK: '#283a4e',
+    WHITE: '#e6ecf2',
+    PINK: '#c43bbe',
+}
